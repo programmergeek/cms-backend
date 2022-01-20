3b8f37ba5d31c3957e984b0038073ad93ddd3a11
programmergeek <programergeek@gmail.com>
2022-01-20 17:57:03 +0200

    create function to return all posts from firestore

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,15 +1,19 @@
-import { getFirestore, collection, addDoc } from 'firebase/firestore'
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore'
+import { firebaseApp } from './firebaseInit'
 
-const firestore = getFirestore()
+const firestore = getFirestore(firebaseApp)
 
-export const createRecord = ( _title:string, _author:string, _content:string, _date:Date, _contentID:string) => (
+export const createRecord = ( _title:string, _author:string, _content:string, _date:Date, _contentID:string) => {
     addDoc(collection(firestore, "Posts"), {
         title: _title,
         author: _author,
         date: _date,
         content: _content,
         contentID: _contentID
+    })
+}
 
-})
-
-)
+export const getPosts = async () => {
+    const posts = await getDocs(collection(firestore, "Posts"))
+    return posts.docs
+}
