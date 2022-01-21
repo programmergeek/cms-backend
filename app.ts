7034ccaf444588e79f5fe486d41ef14860a321a7
programmergeek <programergeek@gmail.com>
2022-01-21 13:03:02 +0200

    changed port

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express()
-const PORT = 8080
+const PORT = 8000
 app.get('/', (req, res) => res.send('Express + TypeScript Server'))
 app.listen(PORT, () => {
     console.log(`⚡[server]: is running at https://localhost:${PORT}⚡`)
