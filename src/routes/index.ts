3ec26bc3a60821bec686ec08b98899d50be0fcf5
programmergeek <programergeek@gmail.com>
2022-01-22 01:46:58 +0200

    removed redirects

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,14 +15,11 @@ router.get("/post/:content_id", function(req, res, next){ res.send("Sparta")})
 
 // create a new post
 router.post("/create", function(req, res, next){
-    createPost(req.body["title"],req.body["author"],req.body["content"], getTodaysDate(), generateContentId()).then(() => {
-        res.redirect("/")
-    })
-})
+    createPost(req.body["title"],req.body["author"],req.body["content"], getTodaysDate(), generateContentId())})
 
 // edit a post
 router.post("/edit/:content_id", async function(req, res, next){
-    await updatePost(req.body["title"], req.body["author"], req.body["content"], req.body["publish_date"], req.params.content_id).then(() => res.redirect(`/post/${req.params.content_id}`))
+    await updatePost(req.body["title"], req.body["author"], req.body["content"], req.body["publish_date"], req.params.content_id)
 })
 
 // delete a post
