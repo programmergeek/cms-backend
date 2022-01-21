392d15582f1df8db31c37619abd99eb1e94202d4
programmergeek <programergeek@gmail.com>
2022-01-21 18:53:45 +0200

    test getPosts

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import { getPosts } from './src/firebase/firestore'
 
 const app = express()
 const PORT = 8000
-app.get('/', (req, res) => res.send('Express + TypeScript Server'))
+app.get('/', async (req, res) => res.send( await getPosts().then(results => results)))
 app.listen(PORT, () => {
     console.log(`⚡[server]: is running at https://localhost:${PORT}⚡`)
 })
